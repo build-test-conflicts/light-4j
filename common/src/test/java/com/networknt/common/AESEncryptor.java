ab18df70e41dc8d0f75afa29c060e155ece6ba31
build-test-conflicts <build-test-conflicts@gmail.com>
2020-02-18 10:51:54 +0100

    'Trigger build #33e507b'

--- common/src/test/java/com/networknt/common/AESEncryptor.java
+++ common/src/test/java/com/networknt/common/AESEncryptor.java
@@ -1,31 +1,12 @@
-/*
- * Copyright (c) 2016 Network New Technologies Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.networknt.common;
-
 import static com.networknt.decrypt.Decryptor.CRYPT_PREFIX;
 import static java.lang.System.exit;
-
 import java.io.UnsupportedEncodingException;
 import java.security.AlgorithmParameters;
 import java.security.InvalidKeyException;
 import java.security.spec.InvalidParameterSpecException;
 import java.security.spec.KeySpec;
 import java.util.Base64;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -34,81 +15,69 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-
 import com.networknt.utility.Constants;
-
-
 public class AESEncryptor {
-    public static void main(String [] args) {
-        if(args.length == 0) {
-            System.out.println("Please provide plain text to encrypt!");
-            exit(0);
-        }
-        AESEncryptor encryptor = new AESEncryptor();
-        System.out.println(encryptor.encrypt(args[0]));
+  public static void main(  String[] args){
+    if (args.length == 0) {
+      System.out.println("Please provide plain text to encrypt!");
+      exit(0);
     }
-
-    private static final int ITERATIONS = 65536;
-    private static final int KEY_SIZE = 128;
-    private static final byte[] SALT = { (byte) 0x0, (byte) 0x0, (byte) 0x0, (byte) 0x0, (byte) 0x0, (byte) 0x0, (byte) 0x0, (byte) 0x0 };
-    private static final String STRING_ENCODING = "UTF-8";
-    private SecretKeySpec secret;
-    private Cipher cipher;
-    private Base64.Encoder base64Encoder;
-
-    public AESEncryptor() {
-        try {
-           /* Derive the key, given password and salt. */
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            KeySpec spec;
-
-            spec = new PBEKeySpec(Constants.FRAMEWORK_NAME.toCharArray(), SALT, ITERATIONS, KEY_SIZE);
-            SecretKey tmp = factory.generateSecret(spec);
-            secret = new SecretKeySpec(tmp.getEncoded(), "AES");
-
-            // CBC = Cipher Block chaining
-            // PKCS5Padding Indicates that the keys are padded
-            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-
-            // For production use commons base64 encoder
-            base64Encoder = Base64.getEncoder();
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to initialize", e);
-        }
+    AESEncryptor encryptor=new AESEncryptor();
+    System.out.println(encryptor.encrypt(args[0]));
+  }
+  private static int ITERATIONS=65536;
+  private static int KEY_SIZE=128;
+  private static byte[] SALT={(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0};
+  private static String STRING_ENCODING="UTF-8";
+  private SecretKeySpec secret;
+  private Cipher cipher;
+  private Base64.Encoder base64Encoder;
+  public AESEncryptor(){
+    try {
+      SecretKeyFactory factory=SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+      KeySpec spec;
+      spec=new PBEKeySpec(Constants.FRAMEWORK_NAME.toCharArray(),SALT,ITERATIONS,KEY_SIZE);
+      SecretKey tmp=factory.generateSecret(spec);
+      secret=new SecretKeySpec(tmp.getEncoded(),"AES");
+      cipher=Cipher.getInstance("AES/CBC/PKCS5Padding");
+      base64Encoder=Base64.getEncoder();
     }
-
-    /**
-     * Encrypt given input string
-     *
-     * @param input
-     * @return
-     * @throws RuntimeException
-     */
-    public String encrypt(String input)
-    {
-        try
-        {
-            byte[] inputBytes = input.getBytes(STRING_ENCODING);
-            // CBC = Cipher Block chaining
-            // PKCS5Padding Indicates that the keys are padded
-            cipher.init(Cipher.ENCRYPT_MODE, secret);
-            AlgorithmParameters params = cipher.getParameters();
-            byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
-            byte[] ciphertext = cipher.doFinal(inputBytes);
-            byte[] out = new byte[iv.length + ciphertext.length];
-            System.arraycopy(iv, 0, out, 0, iv.length);
-            System.arraycopy(ciphertext, 0, out, iv.length, ciphertext.length);
-            return CRYPT_PREFIX + ":" + base64Encoder.encodeToString(out);
-        } catch (IllegalBlockSizeException e) {
-            throw new RuntimeException("Unable to encrypt", e);
-        } catch (BadPaddingException e) {
-            throw new RuntimeException("Unable to encrypt", e);
-        } catch (InvalidKeyException e) {
-            throw new RuntimeException("Unable to encrypt", e);
-        } catch (InvalidParameterSpecException e) {
-            throw new RuntimeException("Unable to encrypt", e);
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("Unable to encrypt", e);
-        }
+ catch (    Exception e) {
+      throw new RuntimeException("Unable to initialize",e);
     }
+  }
+  /** 
+ * Encrypt given input string
+ * @param input
+ * @return
+ * @throws RuntimeException
+ */
+  public String encrypt(  String input){
+    try {
+      byte[] inputBytes=input.getBytes(STRING_ENCODING);
+      cipher.init(Cipher.ENCRYPT_MODE,secret);
+      AlgorithmParameters params=cipher.getParameters();
+      byte[] iv=params.getParameterSpec(IvParameterSpec.class).getIV();
+      byte[] ciphertext=cipher.doFinal(inputBytes);
+      byte[] out=new byte[iv.length + ciphertext.length];
+      System.arraycopy(iv,0,out,0,iv.length);
+      System.arraycopy(ciphertext,0,out,iv.length,ciphertext.length);
+      return CRYPT_PREFIX + ":" + base64Encoder.encodeToString(out);
+    }
+ catch (    IllegalBlockSizeException e) {
+      throw new RuntimeException("Unable to encrypt",e);
+    }
+catch (    BadPaddingException e) {
+      throw new RuntimeException("Unable to encrypt",e);
+    }
+catch (    InvalidKeyException e) {
+      throw new RuntimeException("Unable to encrypt",e);
+    }
+catch (    InvalidParameterSpecException e) {
+      throw new RuntimeException("Unable to encrypt",e);
+    }
+catch (    UnsupportedEncodingException e) {
+      throw new RuntimeException("Unable to encrypt",e);
+    }
+  }
 }
--- utility/src/main/java/com/networknt/utility/FingerPrintUtil.java
+++ utility/src/main/java/com/networknt/utility/FingerPrintUtil.java
@@ -1,54 +1,35 @@
-/*
- * Copyright (c) 2016 Network New Technologies Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.networknt.utility;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-
 import java.security.MessageDigest;
 import java.security.cert.Certificate;
-
 public class FingerPrintUtil {
-    static final Logger logger = LoggerFactory.getLogger(CodeVerifierUtil.class);
-    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
-
-    public static String getCertFingerPrint(Certificate cert)  {
-        byte [] digest = null;
-        try {
-            byte[] encCertInfo = cert.getEncoded();
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
-            digest = md.digest(encCertInfo);
-        } catch (Exception e) {
-            logger.error("Exception:", e);
-        }
-        if (digest != null) {
-            return bytesToHex(digest).toLowerCase();
-        }
-        return null;
+  static Logger logger=LoggerFactory.getLogger(CodeVerifierUtil.class);
+  private static char[] hexArray="0123456789ABCDEF".toCharArray();
+  public static String getCertFingerPrint(  Certificate cert){
+    byte[] digest=null;
+    try {
+      byte[] encCertInfo=cert.getEncoded();
+      MessageDigest md=MessageDigest.getInstance("SHA-1");
+      digest=md.digest(encCertInfo);
     }
-
-    private static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
+ catch (    Exception e) {
+      logger.error("Exception:",e);
     }
+    if (digest != null) {
+      return bytesToHex(digest).toLowerCase();
+    }
+    return null;
+  }
+  public static String bytesToHex(  byte[] bytes){
+    char[] hexChars=new char[bytes.length * 2];
+    for (int j=0; j < bytes.length; j++) {
+      int v=bytes[j] & 0xFF;
+      hexChars[j * 2]=hexArray[v >>> 4];
+      hexChars[j * 2 + 1]=hexArray[v & 0x0F];
+    }
+    return new String(hexChars);
+  }
+  public FingerPrintUtil(){
+  }
 }
