cca8638b00ae4f35334833afea46e82292b3f999
build-test-conflicts <build-test-conflicts@gmail.com>
2020-02-18 10:51:43 +0100

    'Trigger build #5ed511a3ea6af74f832663109b5a9adc5df1fa07'

--- info/src/test/java/com/networknt/info/ServerInfoDisabledTest.java
+++ info/src/test/java/com/networknt/info/ServerInfoDisabledTest.java
@@ -1,21 +1,4 @@
-/*
- * Copyright (c) 2016 Network New Technologies Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.networknt.info;
-
 import com.networknt.client.Http2Client;
 import com.networknt.config.Config;
 import com.networknt.exception.ClientException;
@@ -37,7 +20,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xnio.IoUtils;
 import org.xnio.OptionMap;
-
 import java.io.File;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
@@ -49,99 +31,83 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
-
-/**
+/** 
  * Created by steve on 01/10/16.
  */
 public class ServerInfoDisabledTest {
-    static final Logger logger = LoggerFactory.getLogger(ServerInfoGetHandlerTest.class);
-
-    static Undertow server = null;
-    static String homeDir = System.getProperty("user.home");
-
-    @BeforeClass
-    public static void setUp() throws Exception {
-        // inject in memory constructed info.json to homeDir as classpath
-        Config.getInstance().clear();
-        Map<String, Object> map = new HashMap<>();
-        map.put("enableServerInfo", false);
-        Config.getInstance().getYaml().dump(map, new PrintWriter(new File(homeDir + "/info.yml"), Charset.defaultCharset().name()));
-        // Add home directory to the classpath of the system class loader.
-        addURL(new File(homeDir).toURI().toURL());
-
-        if(server == null) {
-            logger.info("starting server");
-            HttpHandler handler = getTestHandler();
-            server = Undertow.builder()
-                    .addHttpListener(8080, "localhost")
-                    .setHandler(handler)
-                    .build();
-            server.start();
-        }
+  static Logger logger=LoggerFactory.getLogger(ServerInfoGetHandlerTest.class);
+  static Undertow server=null;
+  static String homeDir=System.getProperty("user.home");
+  @BeforeClass public static void setUp() throws Exception {
+    Config.getInstance().clear();
+    Map<String,Object> map=new HashMap<>();
+    map.put("enableServerInfo",false);
+    Config.getInstance().getYaml().dump(map,new PrintWriter(new File(homeDir + "/info.yml"),Charset.defaultCharset().name()));
+    addURL(new File(homeDir).toURI().toURL());
+    if (server == null) {
+      logger.info("starting server");
+      HttpHandler handler=getTestHandler();
+      server=Undertow.builder().addHttpListener(8080,"localhost").setHandler(handler).build();
+      server.start();
     }
-
-    @AfterClass
-    public static void tearDown() throws Exception {
-        if(server != null) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException ignored) {
-
-            }
-            server.stop();
-            logger.info("The server is stopped.");
-        }
-        // Remove the test.json from home directory
-        File configFile = new File(homeDir + "/info.yml");
-        configFile.delete();
-        // this is very important as it impacts subsequent test case if it is not cleared.
-        Config.getInstance().clear();
+  }
+  @AfterClass public static void tearDown() throws Exception {
+    if (server != null) {
+      try {
+        Thread.sleep(100);
+      }
+ catch (      InterruptedException ignored) {
+      }
+      server.stop();
+      logger.info("The server is stopped.");
     }
-
-    static void addURL(URL url) throws Exception {
-        URLClassLoader classLoader
-                = (URLClassLoader) ClassLoader.getSystemClassLoader();
-        Class clazz = URLClassLoader.class;
-        // Use reflection
-        Method method = clazz.getDeclaredMethod("addURL", URL.class);
-        method.setAccessible(true);
-        method.invoke(classLoader, url);
+    File configFile=new File(homeDir + "/info.yml");
+    configFile.delete();
+    Config.getInstance().clear();
+  }
+  public static void addURL(  URL url) throws Exception {
+    URLClassLoader classLoader=(URLClassLoader)ClassLoader.getSystemClassLoader();
+    Class clazz=URLClassLoader.class;
+    Method method=clazz.getDeclaredMethod("addURL",URL.class);
+    method.setAccessible(true);
+    method.invoke(classLoader,url);
+  }
+  public static RoutingHandler getTestHandler(){
+    return Handlers.routing().add(Methods.GET,"/v1/server/info",new ServerInfoGetHandler());
+  }
+  @Test public void testServerInfo() throws Exception {
+    final Http2Client client=Http2Client.getInstance();
+    final CountDownLatch latch=new CountDownLatch(1);
+    final ClientConnection connection;
+    try {
+      connection=client.connect(new URI("http://localhost:8080"),Http2Client.WORKER,Http2Client.SSL,Http2Client.BUFFER_POOL,OptionMap.EMPTY).get();
     }
-
-    static RoutingHandler getTestHandler() {
-        return Handlers.routing().add(Methods.GET, "/v1/server/info", new ServerInfoGetHandler());
+ catch (    Exception e) {
+      throw new ClientException(e);
     }
-
-    @Test
-    public void testServerInfo() throws Exception {
-        final Http2Client client = Http2Client.getInstance();
-        final CountDownLatch latch = new CountDownLatch(1);
-        final ClientConnection connection;
-        try {
-            connection = client.connect(new URI("http://localhost:8080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
-        } catch (Exception e) {
-            throw new ClientException(e);
-        }
-        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
-        try {
-            ClientRequest request = new ClientRequest().setPath("/v1/server/info").setMethod(Methods.GET);
-            request.getRequestHeaders().put(Headers.HOST, "localhost");
-            connection.sendRequest(request, client.createClientCallback(reference, latch));
-            latch.await();
-        } catch (Exception e) {
-            logger.error("Exception: ", e);
-            throw new ClientException(e);
-        } finally {
-            IoUtils.safeClose(connection);
-        }
-        int statusCode = reference.get().getResponseCode();
-        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
-        Assert.assertEquals(404, statusCode);
-        if(statusCode == 404) {
-            Status status = Config.getInstance().getMapper().readValue(body, Status.class);
-            Assert.assertNotNull(status);
-            Assert.assertEquals("ERR10013", status.getCode());
-        }
+    final AtomicReference<ClientResponse> reference=new AtomicReference<>();
+    try {
+      ClientRequest request=new ClientRequest().setPath("/v1/server/info").setMethod(Methods.GET);
+      request.getRequestHeaders().put(Headers.HOST,"localhost");
+      connection.sendRequest(request,client.createClientCallback(reference,latch));
+      latch.await();
     }
-
+ catch (    Exception e) {
+      logger.error("Exception: ",e);
+      throw new ClientException(e);
+    }
+ finally {
+      IoUtils.safeClose(connection);
+    }
+    int statusCode=reference.get().getResponseCode();
+    String body=reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+    Assert.assertEquals(404,statusCode);
+    if (statusCode == 404) {
+      Status status=Config.getInstance().getMapper().readValue(body,Status.class);
+      Assert.assertNotNull(status);
+      Assert.assertEquals("ERR10013",status.getCode());
+    }
+  }
+  public ServerInfoDisabledTest(){
+  }
 }
